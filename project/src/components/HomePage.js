df36c4a0abff0cfdc5bbb2e8e7825da3227afae0
AKSTHIRUMURUGAN <85742080+AKSTHIRUMURUGAN@users.noreply.github.com>
2024-07-27 19:39:48 +0530

    first

--- project/src/components/HomePage.js
+++ project/src/components/HomePage.js
@@ -3,7 +3,7 @@ import React from "react";
 function HomePage() {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <h1 className="bg-blue-500 p-4">Welcome to the Home Page</h1>
     </div>
   );
 }
